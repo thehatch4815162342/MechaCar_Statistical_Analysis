7c2fd7d4123a8aabf277d61ecc3d1bbad3a17c48
thehatch4815162342 <60824995+thehatch4815162342@users.noreply.github.com>
2022-04-13 00:09:34 -0400

    Update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,3 +18,27 @@ total_summary <- suspension_coil_df %>% summarize(Mean=mean(PSI),Median=median(P
 lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
 
 #T-Tests on Suspension Coils
+
+sample_df <- suspension_coil_df %>% sample_n(50)
+
+t.test(sample_df$PSI,mu=mean(suspension_coil_df$PSI))
+
+psi_lot1_sample <- suspension_coil_df %>% subset(Manufacturing_Lot=='Lot1') %>% sample_n(25) 
+t.test(psi_lot1_sample$PSI,mu=mean(suspension_coil_df$PSI)) 
+
+psi_lot2_sample <- suspension_coil_df %>% subset(Manufacturing_Lot=='Lot2') %>% sample_n(25) 
+t.test(psi_lot2_sample$PSI,mu=mean(suspension_coil_df$PSI)) 
+
+psi_lot3_sample <- suspension_coil_df %>% subset(Manufacturing_Lot=='Lot3') %>% sample_n(25)
+t.test(psi_lot3_sample$PSI,mu=mean(suspension_coil_df$PSI))
+
+
+
+
+
+
+
+
+
+
+
